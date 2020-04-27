30efe89de7ae4dd0ad8a0333062e92421ba243a2
sw-ksnu <sw-knu@daum.net>
2020-04-27 14:51:25 +0900

    second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello IoT based Android Lecture!");
+		System.out.println("Hello Department of Software Convergence Engineering!");
 	}
 }
