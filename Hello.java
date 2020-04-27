cdcabb43c8ac17132c0190fa47f0b2860887d2f5
sw-ksnu <63651638+sw-ksnu@users.noreply.github.com>
2020-04-27 15:04:29 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Department of Software Convergence Engineering!");
-		System.out.println("Hello Guys!");
+		System.out.println("Hello World!");
 	}
 }
